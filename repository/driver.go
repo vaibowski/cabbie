627e4d7115c1b6d8a1fa6e4e3326540e3287a7a7
vaibowski <vaibhavvishaliitg@gmail.com>
2024-07-30 15:55:57 +0530

    add logic to store drivers against driverID, set driver location logic

--- repository/driver.go
+++ repository/driver.go
@@ -14,12 +14,21 @@ func (r *DriverRepository) AddDriver(driver models.Driver) error {
 		return errors.New("driver already exists")
 	}
 	r.MapDatastore[driver.Phone] = driver
+	r.MapDatastore[driver.DriverID] = driver
 	return nil
 }
 
 // GetDriverByPhone to be used later on for login
 func (r *DriverRepository) GetDriverByPhone(phone string) (models.Driver, error) {
 	driver, ok := r.MapDatastore[phone]
+	if ok == false {
+		return models.Driver{}, errors.New("driver not found")
+	}
+	return driver, nil
+}
+
+func (r *DriverRepository) GetDriverByDriverID(driverID string) (models.Driver, error) {
+	driver, ok := r.MapDatastore[driverID]
 	if ok == false {
 		return models.Driver{}, errors.New("driver not found")
 	}

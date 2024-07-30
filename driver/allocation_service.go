79e7a3c34499d7dd5e119b4d6fef53f4b278ed0b
vaibowski <vaibhavvishaliitg@gmail.com>
2024-07-30 15:55:37 +0530

    add logic to store drivers against driverID, set driver location logic

--- driver/allocation_service.go
+++ driver/allocation_service.go
@@ -4,6 +4,7 @@ import (
 	"cabbie/models"
 	"errors"
 	"github.com/emirpasic/gods/v2/maps/treemap"
+	"log"
 	"math"
 	"slices"
 )
@@ -11,12 +12,6 @@ import (
 type AllocationService struct {
 	// we will have 4 maps, each corresponding to a unique service type
 	ActiveDriverPool []*treemap.Map[float64, []string]
-}
-
-func NewAllocationService(activeDriverPool []*treemap.Map[float64, []string]) AllocationService {
-	return AllocationService{
-		ActiveDriverPool: activeDriverPool,
-	}
 }
 
 func (svc *AllocationService) AllocateDriver(pickup models.Location, serviceType models.ServiceTypeEnum) (string, error) {
@@ -42,4 +37,41 @@ func (svc *AllocationService) AllocateDriver(pickup models.Location, serviceType
 	driverIDList = slices.Delete(driverIDList, 0, 1)
 	svc.ActiveDriverPool[serviceType].Put(target, driverIDList)
 	return driverID, nil
+}
+
+func (svc *AllocationService) SetLocation(driverID string, serviceType models.ServiceTypeEnum, location models.Location) {
+	driversAtLocation, found := svc.ActiveDriverPool[serviceType].Get(location.XCoordinate)
+	if found != true || driversAtLocation == nil {
+		var driverList []string
+		driverList = append(driverList, driverID)
+		svc.ActiveDriverPool[serviceType].Put(location.XCoordinate, driverList)
+	} else {
+		driversAtLocation = append(driversAtLocation, driverID)
+		svc.ActiveDriverPool[serviceType].Put(location.XCoordinate, driversAtLocation)
+	}
+	return
+}
+
+func (svc *AllocationService) UnsetLocation(driverID string, serviceType models.ServiceTypeEnum, lastLocation models.Location) error {
+	driverListAtLocation, found := svc.ActiveDriverPool[serviceType].Get(lastLocation.XCoordinate)
+	if found != true || driverListAtLocation == nil {
+		log.Printf("no drivers listed at this location: %f", lastLocation)
+		return errors.New("no drivers listed at this location")
+	} else {
+		driverIndex := slices.Index(driverListAtLocation, driverID)
+		if driverIndex == -1 {
+			log.Printf("driverID %s not found at previous location %f", driverID, lastLocation)
+			return errors.New("driver not found at previous location")
+		} else {
+			driverListAtLocation = slices.Delete(driverListAtLocation, driverIndex, driverIndex+1)
+			svc.ActiveDriverPool[serviceType].Put(lastLocation.XCoordinate, driverListAtLocation)
+		}
+	}
+	return nil
+}
+
+func NewAllocationService(activeDriverPool []*treemap.Map[float64, []string]) AllocationService {
+	return AllocationService{
+		ActiveDriverPool: activeDriverPool,
+	}
 }

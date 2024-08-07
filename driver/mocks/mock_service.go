0ac7a7896b4d8dabcbe6f27323317a32c02ca6fd
vaibowski <vaibhavvishaliitg@gmail.com>
2024-08-07 17:27:58 +0530

    fix mock file

--- driver/mocks/mock_service.go
+++ driver/mocks/mock_service.go
@@ -14,9 +14,8 @@ func (service *MockService) FetchDriver(driverID string) (models.Driver, error) 
 	return args.Get(0).(models.Driver), args.Error(1)
 }
 
-func (service *MockService) UpdateDriver(driver models.Driver) error {
-	args := service.Called(driver)
-	return args.Error(0)
+func (service *MockService) UpdateDriver(driver models.Driver) {
+	return
 }
 
 func (service *MockService) CreateNewDriver(customer models.Driver) (string, error) {
